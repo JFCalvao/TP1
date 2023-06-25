943c09d1847ff3ab4889e51d93413974b1613787
João Fernando Zocca Calvão de Moura <127261053+JFCalvao@users.noreply.github.com>
2023-06-25 16:46:14 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,10 +1,20 @@
 function aparecerMenu() {
-    let element = document.getElementById("itens");
-    element.removeAttribute("hidden");
-    element.style.display = "flex";
+  let element = document.getElementById("itens");
+  element.removeAttribute("hidden");
+  element.style.display = "flex";
 }
 function sumirMenu() {
-    let element = document.getElementById("itens");
-    element.setAttribute("hidden", "");
-    element.style.display = "none";
+  let element = document.getElementById("itens");
+  element.setAttribute("hidden", "");
+  element.style.display = "none";
+}
+function girarAtomo() {
+    let element = document.getElementById("atomo_legal");
+    element.style.animation = "girando 1.2s ease-in-out 1 forwards";
+    element.style.top = "45vh";
+    setTimeout(naoGirarAtomo, 1200);
+}
+function naoGirarAtomo() {
+    let element = document.getElementById("atomo_legal");
+    element.style.animation = "flutuando 0.7s ease-in-out infinite alternate";
 }
